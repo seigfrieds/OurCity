0002ad2e10ae54a12ea0615140c4829f87f8bce9
Jade Lee <jadelee0924@gmail.com>
2025-10-11 00:01:10 -0500

    Prevent manual ID entry and enable automatic attribute validation

--- server/OurCity.Api/Infrastructure/Database/Post.cs
+++ server/OurCity.Api/Infrastructure/Database/Post.cs
@@ -2,9 +2,9 @@ namespace OurCity.Api.Infrastructure.Database;
 
 public class Post
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Title { get; set; }
-    public string? Description { get; set; }
+    public required string Description { get; set; }
 
     public List<Comment>? Comments { get; set; }
 }
--- server/OurCity.Api/Services/Dtos/PostDto.cs
+++ server/OurCity.Api/Services/Dtos/PostDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OurCity.Api.Services.Dtos;
 
 public class PostDto
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Title is required")]
     public required string Title { get; set; }
-    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Description is required")]
+    public required string Description { get; set; }
 }
