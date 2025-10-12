880fd625248ddd91d485d09dc0430b75c22ff2e4
Jade Lee <jadelee0924@gmail.com>
2025-10-12 13:41:17 -0500

    Add votes, location, and image urls to post

--- server/OurCity.Api/Infrastructure/Database/Post.cs
+++ server/OurCity.Api/Infrastructure/Database/Post.cs
@@ -5,6 +5,8 @@ public class Post
     public int Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
-
-    public List<Comment>? Comments { get; set; }
+    public int Votes { get; set; } = 0;
+    public string Location { get; set; } = "";
+    public List<string>? ImageUrls { get; set; } = new();
+    public List<Comment>? Comments { get; set; } = new();
 }
