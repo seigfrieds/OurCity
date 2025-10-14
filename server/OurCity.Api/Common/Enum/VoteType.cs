7a962f7854f219e3ed5b79c047a67628b7defa9d
Jade Lee <jadelee0924@gmail.com>
2025-10-13 22:33:24 -0500

    document other site usage

--- server/OurCity.Api/Common/Enum/VoteType.cs
+++ server/OurCity.Api/Common/Enum/VoteType.cs
@@ -1,3 +1,6 @@
+/// Following url guides how to convert enum value from integer to specified string in a JSON object
+/// https://stackoverflow.com/questions/2441290/javascriptserializer-json-serialization-of-enum-as-string
+
 using System.Text.Json.Serialization;
 
 namespace OurCity.Api.Common.Enum;
