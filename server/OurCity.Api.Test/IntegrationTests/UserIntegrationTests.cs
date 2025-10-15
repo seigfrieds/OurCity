78ea5d086faff3f42d5b0a26caa77424dcdf3f5d
Andre <106368574+seigfrieds@users.noreply.github.com>
2025-10-15 08:21:23 -0500

    Comments integration tests

---
+++ server/OurCity.Api.Test/IntegrationTests/UserIntegrationTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OurCity.Api.Infrastructure;
+using OurCity.Api.Infrastructure.Database;
+using OurCity.Api.Services;
+using Testcontainers.PostgreSql;
+
+namespace OurCity.Api.Test.IntegrationTests;
+
+public class UserIntegrationTests : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .WithImage("postgres:16.10")
+        .Build();
+    private AppDbContext _dbContext = null!; //null! -> tell compiler to trust it will be initialized
+    private User _testUser = null!;
+
+    public async Task InitializeAsync()
+    {
+        await _postgres.StartAsync();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(_postgres.GetConnectionString())
+            .Options;
+
+        _dbContext = new AppDbContext(options);
+
+        await _dbContext.Database.EnsureCreatedAsync();
+
+        // Seed a generic user for tests
+        _testUser = new User
+        {
+            Id = 1,
+            Username = "Dummy",
+            DisplayName = "Dummy",
+            IsDeleted = false,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+        };
+        _dbContext.Users.Add(_testUser);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _dbContext.DisposeAsync();
+        await _postgres.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task FreshDbShouldReturnNothing()
+    {
+        var postService = new PostService(new PostRepository(_dbContext));
+        var retrievedPost = await postService.GetPosts();
+
+        Assert.True(retrievedPost.IsSuccess);
+        Assert.NotNull(retrievedPost.Data);
+        Assert.Empty(retrievedPost.Data);
+    }
+}
