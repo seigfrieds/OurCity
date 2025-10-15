91e7eba17314089212797dfd2bdde0fa4c40dade
Jade Lee <jadelee0924@gmail.com>
2025-10-14 23:41:15 -0500

    Update imports

--- server/OurCity.Api/Controllers/ExampleController.cs
+++ server/OurCity.Api/Controllers/ExampleController.cs
@@ -1,9 +1,7 @@
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using OurCity.Api.Common.Dtos;
 using OurCity.Api.Common.Dtos.Post;
 using OurCity.Api.Configurations;
 using OurCity.Api.Services;
